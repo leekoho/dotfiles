3664268a5ffb996d8294f05a66e83bce337dbf0e
jia.hao <leekoho95@gmail.com>
2025-03-17 18:16:11 +0800

    Add Nvim config

---
+++ .config/init.lua
@@ -0,0 +1,5 @@
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
+
+vim.api.nvim_command('NvimTreeOpen') -- 启动时自动打开 nvim-tree
---
+++ .config/lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+-- 自动括号
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter", -- 在进入插入模式时加载
+    config = true,
+  },
+}
